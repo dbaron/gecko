27c3549b582d73d37c17878053ac9667b17ff7b9
L. David Baron <dbaron@dbaron.org>
2019-07-15 18:13:58 -0700

    Bug 1561394 - Pass line box through from nsTextFrame::SetLength.

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -8695,7 +8695,10 @@ void nsTextFrame::SetLength(int32_t aLength, nsLineLayout* aLineLayout,
               presContext, this, GetParent());
       nsTextFrame* next = static_cast<nsTextFrame*>(newFrame);
       nsFrameList temp(next, next);
-      GetParent()->InsertFrames(kNoReflowPrincipalList, this, Nothing(), temp);
+      nsLineList::iterator* line =
+          aLineLayout ? aLineLayout->GetLine() : nullptr;
+      GetParent()->InsertFrames(kNoReflowPrincipalList, this,
+                                line ? Some(*line) : Nothing(), temp);
       f = next;
     }
 
