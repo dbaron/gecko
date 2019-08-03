59690375cc411e8185f75b8429c9bb7343865fe2
L. David Baron <dbaron@dbaron.org>
2019-08-02 18:18:33 -0700

    Bug 1571250 - Convert flags passed to ReflowChild, FinishReflowChild, SyncFrameViewAfterReflow and from GetXULLayoutFlags / GetLayoutFlags into an enum class.  r=TYLin
    It seems better to convert this before adding a new flag (in bug
    1547759) and risking replacing the wrong 0 with a flag.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40562

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -4390,8 +4390,10 @@ static nscoord MeasuringReflow(nsIFrame* aChild,
 
   ReflowOutput childSize(childRI);
   nsReflowStatus childStatus;
-  const uint32_t flags = NS_FRAME_NO_MOVE_FRAME | NS_FRAME_NO_SIZE_VIEW |
-                         NS_FRAME_NO_DELETE_NEXT_IN_FLOW_CHILD;
+  const nsIFrame::ReflowChildFlags flags =
+      nsIFrame::ReflowChildFlags::NoMoveFrame |
+      nsIFrame::ReflowChildFlags::NoSizeView |
+      nsIFrame::ReflowChildFlags::NoDeleteNextInFlowChild;
   parent->ReflowChild(aChild, pc, childSize, childRI, wm, LogicalPoint(wm),
                       nsSize(), flags, childStatus);
   nsContainerFrame::FinishReflowChild(aChild, pc, childSize, &childRI, wm,
@@ -6170,7 +6172,7 @@ void nsGridContainerFrame::ReflowInFlowChild(
   ReflowOutput childSize(childRI);
   const nsSize dummyContainerSize;
   ReflowChild(aChild, pc, childSize, childRI, childWM, LogicalPoint(childWM),
-              dummyContainerSize, 0, aStatus);
+              dummyContainerSize, ReflowChildFlags::Default, aStatus);
   LogicalPoint childPos = cb.Origin(wm).ConvertTo(
       childWM, wm, aContainerSize - childSize.PhysicalSize());
   // Apply align/justify-self and reflow again if that affects the size.
@@ -6199,7 +6201,7 @@ void nsGridContainerFrame::ReflowInFlowChild(
 
   childRI.ApplyRelativePositioning(&childPos, aContainerSize);
   FinishReflowChild(aChild, pc, childSize, &childRI, childWM, childPos,
-                    aContainerSize, 0);
+                    aContainerSize, ReflowChildFlags::Default);
   ConsiderChildOverflow(aDesiredSize.mOverflowAreas, aChild);
 }
 
@@ -6730,8 +6732,8 @@ nscoord nsGridContainerFrame::ReflowChildren(GridReflowInput& aState,
   nsReflowStatus ocStatus;
   if (GetPrevInFlow()) {
     ReflowOverflowContainerChildren(PresContext(), *aState.mReflowInput,
-                                    ocBounds, 0, ocStatus,
-                                    MergeSortedFrameListsFor);
+                                    ocBounds, ReflowChildFlags::Default,
+                                    ocStatus, MergeSortedFrameListsFor);
   }
 
   WritingMode wm = aState.mReflowInput->GetWritingMode();
